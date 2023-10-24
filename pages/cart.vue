3d25a6156393a30f6f56f55f1c3a5aa10a7f43b1
chakodingan <salsaproject86@gmail.com>
2023-10-24 10:09:20 +0700

    commit pertama

---
+++ pages/cart.vue
@@ -0,0 +1,8 @@
+<template>
+    <h1><b>Ini Halaman Cart</b></h1>
+</template>
+ <!-- <script setup>
+ definePageMeta({
+    layout:'blank',
+ })
+</script> -->
---
+++ pages/index.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1><b>Ini Halaman Home</b></h1>
+</template>
